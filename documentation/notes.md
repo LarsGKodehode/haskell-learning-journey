50c64bfdee2c41dfa42fbffff09d4ec52540981d
zabronax <104063134+LarsGKodehode@users.noreply.github.com>
2024-08-11 16:08:40 +0000

    split up html creation into multiple functions

--- documentation/notes.md
+++ documentation/notes.md
@@ -11,3 +11,18 @@ Operators in Haskell has some implicit *fixity*, ie they give the left or right 
 ## Formatting
 There does not seem to be any formal styling guides enforced.
 
+## Partial Application
+Haskell functions can be partial applied, specializing it for the concrete case.
+```haskell
+add_ :: Int -> Int -> Int
+add_ x y = x + y
+
+add2_ :: Int -> Int
+add2_ = add_ 2
+
+a :: Int
+a = add2_ 4
+```
+
+## Domain Languages
+More concrete/specific languages that are created from higher languages needs to account for quite a bit more then just narrowing down the functionalities. There might be some better solution for deriving some form of linters and documentation. Machine Learning Models might be provide some solution here for guiding along others, with less domain expertise. 
--- src/blog/main.hs
+++ src/blog/main.hs
@@ -1,18 +1,38 @@
 -- The main entry point of a haskell program
+main :: IO ()
 main = putStrLn html
 
-html = makeHtml "My Title" "hello world!"
+html :: String
+html =
+  makeHtml
+    "The Functional Way"
+    ( h1_ "The Functional Way"
+        <> p_ "Checking this out"
+    )
 
--- The compositional patter in use here seems rather unfortunate
+-- The compositional pattern in use here seems rather unfortunate
 -- will have to research what patterns are common, how flexible the
--- language is and how relationships and bindings can be expressed
-makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+-- language is and how relationships and partial evaluations can be expressed
+makeHtml :: String -> String -> String
+makeHtml title content = "<!DOCTYPE html>" ++ html_ (head_ (title_ title) <> body_ content)
 
-head_ content = "<head>" <> content <> "</head>"
+element :: String -> String -> String
+element tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
-title_ content = "<title>" <> content <> "</title>"
+head_ :: String -> String
+head_ = element "head"
 
-html_ content = "<!DOCTYPE html><html>" <> content <> "</html>"
+title_ :: String -> String
+title_ = element "title"
 
-body_ content = "<body><h1>" <> content <> "</h1></body>"
+html_ :: String -> String
+html_ = element "html"
 
+body_ :: String -> String
+body_ = element "body"
+
+h1_ :: String -> String
+h1_ = element "h1"
+
+p_ :: String -> String
+p_ = element "p"
